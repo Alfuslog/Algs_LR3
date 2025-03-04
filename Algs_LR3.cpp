be972b87dfee8fb282c8801ebbc0fe12ca5870e2
Александр ⠀ <zrohzaev@mail.ru>
2025-03-05 01:24:39 +0300

    макрос)

--- Algs_LR3.cpp
+++ Algs_LR3.cpp
@@ -2,31 +2,44 @@
 #include <iostream>
 using namespace std;
 
-#define IOPUT(type, var, prompt)\  // просто лутшый макрос йоооу
+#define IOPUT(type, var, prompt)\
     cout << prompt;\
-    cin >> var
+    if (!(cin >> var) ){\
+        cout << "\nВведено некорректное значение!\n";\
+        exit(-1);\
+    }
+    
 
+void InputElemsArr() {
 
-void NewPointer(int *p) {
+}
 
+void NewPointer(int *arr, int* ptr) {
+    int i;
+    IOPUT(int, i, "Выберете индекс элемента, указывающего на элемент массива: ");
+
+    *ptr = *arr + i-1;
 }
 
 int main(){
     setlocale(LC_ALL, "RUS");
-    int size, choise;
+    int size, choise, elem;
 
-    IOPUT(int, size, "\nВведите размер массива: ");
+    IOPUT(int, size, "Введите размер массива: ");
     int* arr = new int[size];
 
     IOPUT(int, choise, "\nВведите номер задания: ");
-    if (cin.fail()) {
-        cout << "\nВведено некорректное значение!\n";
-        return 0;
-    }
+
     switch (choise) {
     case(1): {
-        cout << "hi";
-
+        printf("\nЗаполните массив через Enter:\n");
+        for (int i=0; i < size; i++) {
+            cin >> elem;
+            if (cin.fail()) { cout << "\nВведено некорректное значение!\n"; return 0; }
+            
+            InputElemsArr();
+        }
+        
     }
     case 2:{
         return 0;
