8d551eede6f505ea274c439e3d13f58e30261cd0
Александр ⠀ <zrohzaev@mail.ru>
2025-03-05 00:36:25 +0300

    2

--- Algs_LR3.cpp
+++ Algs_LR3.cpp
@@ -1,7 +1,44 @@
-﻿
+﻿#include <stdio.h>
 #include <iostream>
 using namespace std;
-int main()
-{
-    cout << "Hello World!\n";
+
+#define IOPUT(type, var, prompt)\  // просто лутшый макрос йоооу
+    cout << prompt;\
+    cin >> var
+
+
+void NewPointer(int *p) {
+
+}
+
+int main(){
+    setlocale(LC_ALL, "RUS");
+    int size, choise;
+
+    IOPUT(int, size, "\nВведите размер массива: ");
+    int* arr = new int[size];
+
+    IOPUT(int, choise, "\nВведите номер задания: ");
+    if (cin.fail()) {
+        cout << "\nВведено некорректное значение!\n";
+        return 0;
+    }
+    switch (choise) {
+    case(1): {
+        cout << "hi";
+
+    }
+    case 2:{
+        return 0;
+    }
+    default: { cout << "\nНеверный номер задания!\n"; }
+    }
+
+    
+
+
+
+
+
+
 }
