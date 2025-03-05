64797cfd4371a5a94d00779f6508abada8ea29e1
Александр ⠀ <zrohzaev@mail.ru>
2025-03-05 03:22:52 +0300

    всё без задания

--- Algs_LR3.cpp
+++ Algs_LR3.cpp
@@ -8,43 +8,66 @@ using namespace std;
         cout << "\nВведено некорректное значение!\n";\
         exit(-1);\
     }
-    
 
-void InputElemsArr() {
-
+void InputElemsArr(int *arr, int size){
+    printf("\nЗаполните массив через Enter:\n\n");
+    for (int i = 0; i < size; i++) {
+        cout << i + 1; IOPUT(int, arr[i], ")");
+    }
 }
 
-void NewPointer(int *arr, int* ptr) {
-    int i;
-    IOPUT(int, i, "Выберете индекс элемента, указывающего на элемент массива: ");
+void NewPointer(int *arr, int size, int **ptrArr) {
+    for (int i = 0; i < size; i++) {
+        ptrArr[i] = new int(arr[i]);
+    }
+}
 
-    *ptr = *arr + i-1;
+void WritePtr(int** ptrArr, int iPtr, int chislo) {
+    *(ptrArr[iPtr - 1]) = chislo;
+}
+
+void ReadPtr(int** ptrArr, int iPtr) {
+    cout << *(ptrArr[iPtr - 1]);
+}
+
+
+void freePtr(int *arr, int **ptrArr) {
+    delete[] arr;
+    delete[] ptrArr;
 }
 
 int main(){
+    cin.clear();
     setlocale(LC_ALL, "RUS");
-    int size, choise, elem;
+    int size, choise;
 
     IOPUT(int, size, "Введите размер массива: ");
     int* arr = new int[size];
+    InputElemsArr(arr, size);
+
+    int** ptrArr = new int*[size];
+    NewPointer(arr, size, ptrArr);
 
     IOPUT(int, choise, "\nВведите номер задания: ");
-
     switch (choise) {
     case(1): {
-        printf("\nЗаполните массив через Enter:\n");
-        for (int i=0; i < size; i++) {
-            cin >> elem;
-            if (cin.fail()) { cout << "\nВведено некорректное значение!\n"; return 0; }
-            
-            InputElemsArr();
-        }
         
+        for (int i = 0; i < size; i++) {
+            cout << *ptrArr[i] << ' ';
+        }
+
+        
+
+        goto End;
     }
     case 2:{
-        return 0;
+
+        goto End;
     }
     default: { cout << "\nНеверный номер задания!\n"; }
+       End:
+           freePtr(arr, ptrArr);
+           return 0;
     }
 
     
